38d171a49c290f212b922edf4a3fe672be8c3be9
dianfitrip <dianfitri683@gmail.com>
2024-05-28 11:41:22 +0700

    membuat solveMultiWaris. code nya sama dengan multiWaris. hanya di hapus virtual dan Orang di class budi

---
+++ solveMultiWaris/solveMultiWaris.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+
+class Orang {
+public:
+	int umur;
+
+	Orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+	}
+};
+
+class Pekerja :  public Orang {
+public:
+	Pekerja(int pUmur) :
+		Orang(pUmur) {
+		cout << "pekerja dibuat\n" << endl;
+	}
+};
+
+class Pelajar :  public Orang {
+public:
+	Pelajar(int pUmur) :
+		Orang(pUmur)
+	{
+		cout << "pelajar dibuat\n" << endl;
+	}
+};
+
+class Budi : public Pekerja, public Pelajar {
+public:
+
+	Budi(int pUmur) :
+		Pekerja(pUmur),
+		Pelajar(pUmur)		//hal ini dapat dilakukan jika menggunakan virtual
+	{
+		cout << "budi dibuat\n" << endl;
+	}
+};
+
+int main() {
+	Budi a(12);
+
+	return 0;
+}
