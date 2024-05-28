19118d75048ac8970a6305f55f92acd534c29c4b
dianfitrip <dianfitri683@gmail.com>
2024-05-28 10:18:08 +0700

    polimorphy. bikin class seseorang

--- ParadigmaOOPpart2/ParadigmaOOPpart2.cpp
+++ ParadigmaOOPpart2/ParadigmaOOPpart2.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class Seseorang
+{
+public:
+	virtual void pesan() = 0;		//untuk yang ngga menggunakan definisi(pure virtual function)
+
+	//virtual void pesan() {
+	//	cout << "Pesan dari seseorang" << endl;
+	// }								//untuk virtual function yang biasa
+};
+
+
