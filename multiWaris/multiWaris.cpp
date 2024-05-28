ecca2383518d123deb314ac4d6b96febb49e18cd
dianfitrip <dianfitri683@gmail.com>
2024-05-28 11:36:36 +0700

    bikin class pelajar, pekerja dan budi. lalu int main. memanggil

--- multiWaris/multiWaris.cpp
+++ multiWaris/multiWaris.cpp
@@ -13,3 +13,37 @@ public:
 	}
 };
 
+class Pekerja : virtual public Orang {
+public:
+	Pekerja(int pUmur) :
+		Orang(pUmur) {
+		cout << "pekerja dibuat\n" << endl;
+	}
+};
+
+class Pelajar : virtual public Orang {
+public:
+	Pelajar(int pUmur) :
+		Orang(pUmur)
+	{
+		cout << "pelajar dibuat\n" << endl;
+	}
+};
+
+class Budi : public Pekerja, public Pelajar {
+public:
+
+	Budi(int pUmur) :
+		Pekerja(pUmur),
+		Pelajar(pUmur),
+		Orang(pUmur)		//hal ini dapat dilakukan jika menggunakan virtual
+	{
+		cout << "budi dibuat\n" << endl;
+	}
+};
+
+int main() {
+	Budi a(12);
+
+	return 0;
+}
